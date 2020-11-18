28956c86e1c56a1574ec333c3c638284567be4bd
Dimitri <56034538+dima-sw@users.noreply.github.com>
2020-11-18 16:43:24 +0100

    up

--- opfython/POPF_functions/prune.py
+++ opfython/POPF_functions/prune.py
@@ -39,7 +39,7 @@ def prune(opf, X_train, Y_train, X_val, Y_val, tagli, M_loss, n_iterations=10):
 def pruringRun(opf, acc, M_loss, tagli, n_iterations, X_train, Y_train, X_val, Y_val):
     # tmp= accuratezza attuale
     tmp = acc
-    # flag serve per tenere traccia se ci sia ancora un nodo non rilevante
+    # flag serve per tenere traccia se ci sia ancora almeno un nodo non rilevante
     flag = True
 
     # mentre l'accuratezza attuale è >= (dell'accuratezza massima iniziale - M_loss) e mentre ci sta almeno un nodo non Rilevante (flag)
