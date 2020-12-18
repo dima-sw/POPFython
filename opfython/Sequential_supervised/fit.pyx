6969b9a925970b8a64e29deaa60ec6fa19548185
Dimitri <56034538+dima-sw@users.noreply.github.com>
2020-12-18 12:19:17 +0100

    fit comments

--- opfython/Sequential_supervised/fit.pyx
+++ opfython/Sequential_supervised/fit.pyx
@@ -7,10 +7,23 @@ from libc.stdlib cimport malloc, free
 
     
 @cython.boundscheck(False)
-# this disables 'wraparound' indexing from the end of the array using negative
-# indices.
+
 @cython.wraparound(False)
 cpdef fit(nodes,idx_nodes,double [:,:] features,  float Max_Cost):
+
+
+
+
+    """
+        size= numero di nodi
+        P=predecessori
+        L=labels
+        U=used
+        C=costo di ogni nodo
+        idx=Array di nodi ordinato in termini di costo
+        lun=numero di feature di ogni nodo
+
+    """
 	cdef int size=len(features)
 
 	cdef int *P=<int *> malloc(size* sizeof(int))
@@ -24,6 +37,8 @@ cpdef fit(nodes,idx_nodes,double [:,:] features,  float Max_Cost):
 	cdef lun=len(features[0])
 	cdef int s=0
 
+
+    #Inizializzo gli array in base se sono prototipi o meno
 	for i in range(size):
 		U[i]=0
 		if nodes[i].status==1:
@@ -41,12 +56,15 @@ cpdef fit(nodes,idx_nodes,double [:,:] features,  float Max_Cost):
 			L[i]=0
 
 
-
+    #TRAINING, quando sara' finito s sara' -1, il primo s sara' un prototipo siccome ha costo 0
 	while s !=-1:
+
+	    #Setto il nodo come usato, lo aggiungo alla lista ordinata e aggiorno il costo del nodo reale
 		U[s]=1
 		idx_nodes.append(s)
 		nodes[s].cost=C[s]
 
+        #mi prendo il prossimo s
 		s=work(features,s,size,U,L,P,C,lun)
 
 
@@ -65,16 +83,18 @@ cpdef fit(nodes,idx_nodes,double [:,:] features,  float Max_Cost):
 
 
 @cython.boundscheck(False)
-# this disables 'wraparound' indexing from the end of the array using negative
-# indices.
 @cython.wraparound(False)
+#Funzione che fa il training e restituisce il nodo col costo minore
 cdef int work(double [:,:] features,int s, int size,int *U,int *L, int *P, double *C,int lun):
 	cdef int t
 	cdef double curr_cost
 	cdef int s1=-1
 
+    #itero per ogni nodo
 	for t in range(size):
+	    #se s e t non sono uguali
 		if s!=t:
+		    #Se il costo di t e' maggiore del costo di s allora vedo se s possa conquistare t
 			if C[t]>C[s]:
 
 				dist=squared_euclidean_distance(features[t],features[s],lun)
@@ -88,6 +108,7 @@ cdef int work(double [:,:] features,int s, int size,int *U,int *L, int *P, doubl
 					L[t]=L[s]
 					C[t]=curr_cost
 					P[t]=s
+			#Vedo se t possa essere il prossimo nodo ad uscire
 			if (s1==-1 or C[s1]>C[t]):
 				if(U[t]==0):
 					s1=t
