2c350e8303a958badd1f319d3761261b35d2e6b1
Dimitri <56034538+dima-sw@users.noreply.github.com>
2021-01-29 01:01:55 +0100

    Update distancescy.pyx

--- opfython/Seq_CYTHON/distancepx/distancescy.pyx
+++ opfython/Seq_CYTHON/distancepx/distancescy.pyx
@@ -1,13 +1,13 @@
 cimport cython
 
 
-cpdef float squared_euclidean_distance(double [:] x, double [:]  y, int lun):
+cpdef float squared_euclidean_distance(double [:] x, double [:]  y):
     # Calculates the Squared Euclidean distance for each dimension
     #dist = (x - y) ** 2
     
     cdef int i
     cdef float dist=0
-    
+    cdef int lun=len(x)
     for i in range(lun):
         dist+= (x[i]-y[i])*(x[i]-y[i])
 
